b6f484c8d7617d563e3d92a21cc76ba26edabc79
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2022-11-12 21:42:19 +0100

    Created a new custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+	username = None
+	email = models.EmailField(_('email address'), unique=True)
+
+	USERNAME_FIELD: str = 'email'
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
+
+	name = models.CharField(blank=True, max_length=100)
+	
+	def __str__(self):
+		return self.email
